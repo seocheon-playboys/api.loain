baa5aa584aba72153726ffc56e04b4bf98f90e29
kwanjung <kwanjungkim@naver.com>
2024-09-26 16:18:26 +0900

    first commit

---
+++ loain/src/main/java/com/seocheon/loain/LoainApplication.java
@@ -0,0 +1,14 @@
+package com.seocheon.loain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoainApplication.class, args);
+		//test
+	}
+
+}
